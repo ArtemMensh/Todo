3ed507ed81c9cd063c4d471079751a4e7dd7d9ea
Menshakov Artem <artem.menshakov.1999@mail.ru>
2019-12-16 23:17:11 +0300

    sd

--- App/assets/todo.js
+++ App/assets/todo.js
@@ -1,12 +1,16 @@
 
-
+// todo - главный объект, data - информация из local storage 
 var todo = todo || {},
     data = JSON.parse(localStorage.getItem("todoData"));
 
 data = data || {};
 
+
+// самовызывающаяся функция
 (function(todo, data, $) {
  
+	console.log(data)
+
     var defaults = { // селекторы и атрибуты JavaScript, которые будут использованы в функциях
             todoTask: "todo-task",
             todoHeader: "task-header",
@@ -15,8 +19,11 @@ data = data || {};
             taskId: "task-",
             formId: "todo-form",
             dataAttribute: "data",
-            deleteDiv: "delete-div"
-        }, codes = {
+            deleteDiv: "delete-div",
+            color: "color"
+        }
+
+    var codes = {
             "1" : "#pending",  // Для запланированных задач
             "2" : "#inProgress", // Для выполняемых задач
             "3" : "#completed" // Для завершенных задач
@@ -25,8 +32,13 @@ data = data || {};
     todo.init = function (options) {
 
         options = options || {};
+
+        //Функция jQuery.extend() изменяет (расширяет) и возвращает исходный объект, 
+        //дополняя его свойствами объектов источников. 
+        //(Имитация наследования в объектно-ориентированных языках программирования.)
         options = $.extend({}, defaults, options);
 
+        // Перебор элементов в data, calback f обрабатывает каждый элемент
         $.each(data, function (index, params) {
             generateElement(params); 
         });
@@ -48,6 +60,7 @@ data = data || {};
         });*/
 
         // Функция перетаскивания по категориям
+        //index - 1,2,3 value это id
         $.each(codes, function (index, value) {
             $(value).droppable({
                 drop: function (event, ui) {
@@ -100,7 +113,7 @@ data = data || {};
     // Добавляем задачу
     var generateElement = function(params){
         var parent = $(codes[params.code]),
-            wrapper;
+            wrapper; // что такое wrapper
 
         if (!parent) {
             return;
@@ -127,6 +140,8 @@ data = data || {};
             "text": params.description
         }).appendTo(wrapper);
 
+        wrapper[0].style.backgroundColor = params.color
+
         wrapper.draggable({
             start: function() {
                 $("#" + defaults.deleteDiv).show();
@@ -135,7 +150,6 @@ data = data || {};
                 $("#" + defaults.deleteDiv).hide();
             }
         });
-
     };
 
     // Удаляем задачу
@@ -146,15 +160,17 @@ data = data || {};
     todo.add = function() {
         var inputs = $("#" + defaults.formId + " :input"),
             errorMessage = "Заголовок не может быть пустым",
-            id, title, description, date, tempData;
+            id, title, description, date, tempData, color;
 
-        if (inputs.length !== 4) {
+        if (inputs.length !== 5) {
             return;
         }
 
         title = inputs[0].value;
         description = inputs[1].value;
         date = inputs[2].value;
+        color = inputs[3].value;
+
 
         if (!title) {
             generateDialog(errorMessage);
@@ -168,6 +184,7 @@ data = data || {};
             code: "1",
             title: title,
             date: date,
+            color: color,
             description: description
         }
 
@@ -182,6 +199,7 @@ data = data || {};
         inputs[0].value = "";
         inputs[1].value = "";
         inputs[2].value = "";
+        inputs[3].value = "#000000";
     };
 	
 	//выводим сообщение об ошибке, если заголовок пустой
